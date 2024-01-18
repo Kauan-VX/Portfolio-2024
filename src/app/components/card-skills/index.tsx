cc11272f3edca31d523e4f711c2676c79e966be0
Kauan Vieira Xavier <kauanvieiraxavierk@gmail.com>
2024-01-17 22:18:55 -0300

    feat: animation card-skills

--- src/app/components/card-skills/index.tsx
+++ src/app/components/card-skills/index.tsx
@@ -1,33 +1,60 @@
-"use client";
-import { motion } from "framer-motion";
-import Image from "next/image";
+'use client'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { useRef, useState } from 'react'
 
-interface ISkills{
-  imageSrc: string,
-  altText: string,
-  link: string,
-  skillName: string,
-  index: number,
+interface ISkills {
+  imageSrc: string
+  altText: string
+  link: string
+  skillName: string
+  index: number
 }
 
-const CardSkill: React.FC<ISkills> = ({ imageSrc, altText, link, skillName ,index }) => {
+const CardSkill: React.FC<ISkills> = ({
+  imageSrc,
+  altText,
+  link,
+  skillName,
+  index,
+}) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  const mouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (ref.current) {
+      const { clientX, clientY } = e
+      const { width, height, left } = ref.current.getBoundingClientRect()
+      const x = clientX - (left + width / 2)
+      const y = clientY - (left + height / 2)
+      setPosition({ x, y })
+    }
+  }
+
+  const mouseLeave = () => {
+    setPosition({ x: 0, y: 0 })
+  }
+
+  const { x, y } = position
+
   const fadeInAnimationsVariants = {
     initial: {
       opacity: 0,
       y: 100,
     },
-    animate: (index: number) => ( {
+    animate: (index: number) => ({
       opacity: 1,
       y: 0,
       transition: {
         delay: 0.05 * index,
-      
       },
     }),
-  };
+  }
 
   return (
-    <motion.li
+    <motion.div
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
       variants={fadeInAnimationsVariants}
       initial="initial"
       whileInView="animate"
@@ -35,6 +62,9 @@ const CardSkill: React.FC<ISkills> = ({ imageSrc, altText, link, skillName ,inde
         once: true,
       }}
       custom={index}
+      ref={ref}
+      animate={{ x, y }}
+      transition={{ type: 'spring', stiffness: 150, damping: 15, mass: 0.1 }}
     >
       <a className="link-skills whitespace-nowrap" target="_blank" href={link}>
         <Image
@@ -46,7 +76,7 @@ const CardSkill: React.FC<ISkills> = ({ imageSrc, altText, link, skillName ,inde
         />
         {skillName}
       </a>
-    </motion.li>
-  );
-};
-export default CardSkill;
+    </motion.div>
+  )
+}
+export default CardSkill
--- src/app/presentation/page.tsx
+++ src/app/presentation/page.tsx
@@ -1,14 +1,13 @@
-"use client"
-import { motion } from "framer-motion";
-import { DevIcon } from "../components/icons/presentation/dev";
-import { ScrollIcon } from "../components/icons/presentation/scroll";
+'use client'
+import { motion } from 'framer-motion'
+import { DevIcon } from '../components/icons/presentation/dev'
+import { ScrollIcon } from '../components/icons/presentation/scroll'
+import Typewriter from 'typewriter-effect'
 
-export default function Presentation(){
-
-
+export default function Presentation() {
   const textVariants = {
     initialLeft: {
-      x : -500,
+      x: -500,
       opacity: 0,
     },
     animateLeft: {
@@ -16,8 +15,8 @@ export default function Presentation(){
       opacity: 1,
       transition: {
         duration: 1,
-        staggerChildren:1,
-      }
+        staggerChildren: 1,
+      },
     },
     initialRight: {
       opacity: 0,
@@ -26,36 +25,72 @@ export default function Presentation(){
       opacity: 1,
       transition: {
         duration: 2,
-        staggerChildren:2,
-      }
+        staggerChildren: 2,
+      },
     },
     scrollButton: {
-      opacity : 0 ,
+      opacity: 0,
       y: 10,
       transition: {
-        duration: 3 ,
+        duration: 3,
         repeat: Infinity,
-        
-            }
-    }
+      },
+    },
   }
-  return(
-    <section className=" h-[100vh]  dark:bg-dark-background bg-light-background p-6" >
+  return (
+    <section className=" h-[100vh]  dark:bg-dark-background bg-light-background p-6">
       <div className="flex">
-      <motion.header  className="px-8 w-2/4 header-presentation flex w-full justify-center flex-col" variants={textVariants} initial="initialLeft" animate="animateLeft">
-        <motion.h1 variants={textVariants} className="flex   text-3xl"> <b className="flex dark:text-dark-text text-light-text">Olá! Eu sou o </b> <b className=" flex items-center gap-2 text-transparent font-bold bg-gradient-to-r from-purple via-pink to-yellow bg-clip-text animate-gradient ml-2"> Kauan Xavier</b></motion.h1>
-        <motion.h2 variants={textVariants} className="dark:text-dark-text text-light-text"><strong className="text-4xl">Desenvolvedor Frond-End</strong></motion.h2>
-        <motion.span className="w-full max-w-10"  variants={textVariants} animate="scrollButton"> 
-        <ScrollIcon/>
-        </motion.span>
-      {/* <p className="dark:text-dark-text text-light-text">tenho <span>1 ano(s)</span> de experiência</p>  */}
-      </motion.header>
-      <motion.div className="flex justify-center w-2/4 " variants={textVariants} initial="initialRight" animate="animateRight">
-      <DevIcon/>
-      </motion.div>
+        <motion.header
+          className="px-8  header-presentation flex w-full justify-center flex-col"
+          variants={textVariants}
+          initial="initialLeft"
+          animate="animateLeft"
+        >
+          <motion.h1 variants={textVariants} className="flex   text-3xl">
+            {' '}
+            <b className="flex dark:text-dark-text text-light-text">
+              Olá! Eu sou o{' '}
+            </b>{' '}
+            <b className=" flex items-center gap-2 text-transparent font-bold bg-gradient-to-r from-purple via-pink to-yellow bg-clip-text animate-gradient ml-2">
+              <Typewriter
+                options={{
+                  strings: [
+                    'Kauan',
+                    'Kauan Vieira',
+                    'Kauan Xavier',
+                    'Kauan Dev',
+                  ],
+                  autoStart: true,
+                  loop: true,
+                  delay: 100,
+                }}
+              />
+            </b>
+          </motion.h1>
+          <motion.h2
+            variants={textVariants}
+            className="dark:text-dark-text text-light-text"
+          >
+            <strong className="text-4xl">Desenvolvedor Frond-End</strong>
+          </motion.h2>
+          <motion.span
+            className="w-full max-w-10"
+            variants={textVariants}
+            animate="scrollButton"
+          >
+            <ScrollIcon />
+          </motion.span>
+          {/* <p className="dark:text-dark-text text-light-text">tenho <span>1 ano(s)</span> de experiência</p>  */}
+        </motion.header>
+        <motion.div
+          className="flex justify-center w-2/4 "
+          variants={textVariants}
+          initial="initialRight"
+          animate="animateRight"
+        >
+          <DevIcon />
+        </motion.div>
       </div>
-
-
     </section>
   )
 }
