1216ed052647b46628550d0db0bd8d8b4aa7c0de
Kauan Vieira Xavier <kauanvieiraxavierdev@gmail.com>
2024-03-12 13:44:50 -0300

    fix: text adjust

--- src/app/constants/experience.ts
+++ src/app/constants/experience.ts
@@ -3,9 +3,9 @@ import { IExperience } from '../components/card-experience'
 export const EXPERIENCE: IExperience[] = [
   {
     company_name: `Noclaf`,
-    initial_date: `Noclaf`,
-    finish_date: `Noclaf`,
-    job_title: `Noclaf`,
+    initial_date: `05/2023`,
+    finish_date: `Presente`,
+    job_title: `Desenvolvedor Front-End Júnior`,
     job_description: [
       `Recriando interfaces criadas pelo time de UX/UI, seja no Figma ou XdAdobe.`,
       `Usando o framework Angular10+ com SCSS , RxJs e Angular Material.`,
