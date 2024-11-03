e4245d3392edaca93913485f33f0d47cabf2059a
Kauan Vieira Xavier <kauanvieiraxavierk@gmail.com>
2024-11-03 13:26:39 -0300

    feat: new certificates

--- public/constants/certificate.ts
+++ public/constants/certificate.ts
@@ -6,5 +6,13 @@ export const translateCertificate = (t: Function): ICertificate[] => {
       platform: "Udemy",
       title: t("certificate.react_native.title"),
     },
+    {
+      platform: "Udemy",
+      title: t("certificate.java_script.title"),
+    },
+    {
+      platform: "Udemy",
+      title: t("certificate.nest_js.title"),
+    },
   ];
 };
